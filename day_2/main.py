c99f5a0bb53cf38624783653b8132567504c6f63
MarcinGladkowski <marcingladkowski@gmail.com>
2023-12-05 20:21:03 +0100

    day_1: parse cube

--- day_2/main.py
+++ day_2/main.py
@@ -2,8 +2,8 @@ from dataclasses import dataclass
 import re
 from enum import Enum
 
-
 TEST_DATA_1 = 'input_test.txt'
+
 
 def load_data(file_name: str) -> list:
     with open(file_name) as f:
@@ -12,10 +12,12 @@ def load_data(file_name: str) -> list:
 
 test_data = load_data(TEST_DATA_1)
 
+
 class Color(Enum):
     RED = 'red'
     GREEN = 'green'
-    BLUE = 'blue'
+    BLUE = 'glue'
+
 
 @dataclass
 class Game:
@@ -30,7 +32,6 @@ class Set:
         pass
 
 
-
 def get_game_id(game_title: str):
     result = re.search('\d', game_title)
 
@@ -41,9 +42,7 @@ def get_game_id(game_title: str):
 
 
 def get_sets(sets: str):
-
     sets = sets.split(";")
-
     print(sets)
 
 
@@ -52,7 +51,27 @@ for plain_game in test_data:
     title, cubes = plain_game.split(':')
 
     get_game_id(title)
-
     get_sets(cubes)
 
 
+class Cube:
+
+    def __init__(self, color: Color, value: int):
+        super().__init__()
+        self.color = color
+        self.value = value
+
+    @classmethod
+    def from_plain(cls, input: str):
+        matches = re.search('(\w+)\s{1}(\d{1,2})', input)
+
+        if matches is None:
+            raise ValueError("Cannot parse cube")
+
+        return cls(Color[matches.group(1).upper()], int(matches.group(2)))
+
+
+red_6 = Cube.from_plain('Red 6')
+
+assert red_6.color == Color.RED
+assert red_6.value == 6
