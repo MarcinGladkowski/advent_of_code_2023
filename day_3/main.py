0a27e4f83ec33fce1f509db311826c9f324230ff
MarcinGladkowski <marcingladkowski@gmail.com>
2023-12-06 13:33:07 +0100

    day_3(part_1): get number element positions

--- day_3/main.py
+++ day_3/main.py
@@ -1,3 +1,4 @@
+import re
 from enum import Enum
 
 TEST_DATA = 'input_test.txt'
@@ -24,12 +25,40 @@ part_testing = [
     ['#...']
 ]
 
-def get_numbers_with_symbols(data: list):
 
+class Point:
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"{self.x} - {self.y}"
+
+
+def get_number_positions(line_index: int, line: str):
+    numbers = re.findall(r'\d+', line)
+
+    numbers_positions = {}
+
+    for number in numbers:
+        numbers_positions[number] = []
+
+        start = line.find(number)
+
+        for _ in number:
+            numbers_positions[number].append(Point(line_index, start))
+
+    return numbers_positions
+
+
+print(get_number_positions(0, '...10..456...'))
+
+def get_numbers_with_symbols(data: list):
     for idx, row in enumerate(data):
         print(row)
+
 
 # test result = 4361
 
 get_numbers_with_symbols(test_data)
-
