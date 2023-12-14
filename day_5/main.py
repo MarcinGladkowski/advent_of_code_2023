cd81705cbc9c6a54a0167198f3326b3719d5c19f
MarcinGladkowski <marcingladkowski@gmail.com>
2023-12-14 06:58:10 +0100

    day_5(part_1): first test for first soil

--- day_5/main.py
+++ day_5/main.py
@@ -4,8 +4,6 @@ test_data = load_data('input_test.txt')
 
 sanitize = [x for x in test_data if x != '']
 
-print(sanitize)
-
 
 def parse_input(data: list) -> list:
     operations = {
@@ -22,17 +20,52 @@ def parse_input(data: list) -> list:
             operations['seeds'] = line
 
 
-class ComparePlants:
-    def __init__(self, name: str):
-        self.name = name
-
-
 class Range:
 
     def __init__(self, start: int, step: int):
         self.step = step
         self.start = start
 
+    def in_range(self, test_seed: int):
+        if self.start <= test_seed <= self.start + self.step:
+            return True
 
-range_1 = Range(98, 2)
-range_2 = Range(50, 48)
+        return False
+
+    def diff_start(self, range) -> bool:
+        return range.start - self.start
+
+
+class Category:
+    def __init__(self, ranges: list[Range]):
+        self.ranges = ranges
+
+    def get_range(self, index: int):
+        return self.ranges[index]
+
+
+'''
+    seed-to-soil solution
+
+    Test for seed 79    
+'''
+base = Category([Range(98, 2), Range(50, 48)])
+destination = Category([Range(50, 2), Range(52, 48)])
+
+
+def calculate_destination(base: Category, destination: Category, test_seed: int) -> int:
+    for i, range in enumerate(base.ranges):
+        if range.in_range(test_seed):
+            # got range
+            diff = range.diff_start(destination.get_range(i))
+
+            return test_seed + diff
+
+    return test_seed
+
+
+assert 81 == calculate_destination(base,destination,79)
+assert 14 == calculate_destination(base,destination,14)
+assert 57 == calculate_destination(base,destination,55)
+assert 13 == calculate_destination(base,destination,13)
+
