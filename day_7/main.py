f69277c37d561aa903db99529e196450a64924be
MarcinGladkowski <marcingladkowski@gmail.com>
2023-12-16 16:58:55 +0100

    day_7(part_1): card comparator

--- day_7/main.py
+++ day_7/main.py
@@ -2,7 +2,6 @@ import operator
 from enum import Enum
 from collections import Counter
 
-
 cards = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
 
 
@@ -43,6 +42,7 @@ class CardSet:
     def _occurences_of_same_cards_count(self, occurence_of_combination: int, count: int = 1):
         return operator.countOf(list(Counter(self.set).values()), occurence_of_combination) == count
 
+
 '''
 32T3K 765
 T55J5 684
@@ -64,3 +64,26 @@ assert Type.TWO_PAIR == CardSet('KK677', 765).get_type()
 assert Type.FOUR == CardSet('AA7AA', 765).get_type()
 assert Type.FIVE == CardSet('TTTTT', 765).get_type()
 assert Type.HIGH_CARD == CardSet('23456', 765).get_type()
+
+
+class CardSetsComparator:
+    @staticmethod
+    def compare(set_a: CardSet, set_b: CardSet):
+        """
+            Is the set_a > than set_b
+            The same set - compare cards one by one
+            Not the same - compare Type
+        """
+        if set_a.get_type() == set_b.get_type():
+            """Compare cards"""
+            for i in range(5):
+                if set_a.set[i] == set_b.set[i]:
+                    continue
+
+                return cards.index(set_a.set[i]) > cards.index(set_b.set[i])
+
+        return set_a.get_type().value > set_b.get_type().value
+
+
+assert CardSetsComparator.compare(CardSet('TTTTT', 765), CardSet('32T3K', 765))
+assert CardSetsComparator.compare(CardSet('KK677', 765), CardSet('KTJJT', 765))
