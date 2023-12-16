19b2c59dac6abfb424199c20a77110bcc4b2ea75
MarcinGladkowski <marcingladkowski@gmail.com>
2023-12-16 13:33:33 +0100

    day_6(part_1): rest tests for test races

--- day_6/main.py
+++ day_6/main.py
@@ -4,17 +4,34 @@ class Race:
         self.time = time
 
 
-def is_win_for_hold(hold: int, race: Race) -> bool:
-    time_to_ride = race.time - hold
-    ride_distance = hold * time_to_ride
+class RaceWinCalculator:
 
-    return ride_distance > race.distance
+    @staticmethod
+    def count_win_holds(race: Race):
+        count = 0
+        for time_hold in range(race.time + 1):
+            if RaceWinCalculator.is_win_for_hold(time_hold, race):
+                count += 1
+
+        return count
+
+    @staticmethod
+    def is_win_for_hold(hold: int, race: Race) -> bool:
+        time_to_ride = race.time - hold
+        ride_distance = hold * time_to_ride
+
+        return ride_distance > race.distance
 
 
-assert False == is_win_for_hold(1, Race(7, 9))
-assert is_win_for_hold(2, Race(7, 9))
-assert is_win_for_hold(3, Race(7, 9))
-assert is_win_for_hold(4, Race(7, 9))
-assert is_win_for_hold(5, Race(7, 9))
-assert False == is_win_for_hold(6, Race(7, 9))
-assert False == is_win_for_hold(7, Race(7, 9))
+assert False == RaceWinCalculator.is_win_for_hold(1, Race(7, 9))
+assert RaceWinCalculator.is_win_for_hold(2, Race(7, 9))
+assert RaceWinCalculator.is_win_for_hold(3, Race(7, 9))
+assert RaceWinCalculator.is_win_for_hold(4, Race(7, 9))
+assert RaceWinCalculator.is_win_for_hold(5, Race(7, 9))
+assert False == RaceWinCalculator.is_win_for_hold(6, Race(7, 9))
+assert False == RaceWinCalculator.is_win_for_hold(7, Race(7, 9))
+
+
+assert 4 == RaceWinCalculator.count_win_holds(Race(7, 9))
+assert 8 == RaceWinCalculator.count_win_holds(Race(15, 40))
+assert 9 == RaceWinCalculator.count_win_holds(Race(30, 200))
