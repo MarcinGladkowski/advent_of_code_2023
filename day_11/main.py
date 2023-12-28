47f7d9ffe8cbc0e7e4f8f0daf23ec8e45389e21c
MarcinGladkowski <marcingladkowski@gmail.com>
2023-12-28 12:37:09 +0100

    day_11(part_1): expand universum for test data

--- day_11/main.py
+++ day_11/main.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from shared.main import load_data
 
 test_universum_raw = load_data('test_input.txt')
@@ -13,20 +15,67 @@ def parse(raw: list) -> list:
 test_galaxy = parse(test_universum_raw)
 
 
-def any_galaxies_horizontally(universum_row: list) -> bool:
-    return len(set(universum_row)) == 1
+def has_galaxies(universum_row: list) -> bool:
+    return len(set(universum_row)) > 1
 
 
-assert any_galaxies_horizontally(['.', '.'])
-assert any_galaxies_horizontally(['.', '#']) == False
+def expand_row(universum_row: list, expanders: list, expander_sign: str = '.') -> list:
+    for i, expander in enumerate(expanders):
+        universum_row.insert(expander + i, expander_sign)
+
+    return universum_row
+
+
+assert ['.', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.', '.'] == expand_row(
+    ['.', '.', '.', '#', '.', '.', '.', '.', '.', '.'], [2, 5, 8])
+
+assert has_galaxies(['.', '.']) == False
+assert has_galaxies(['.', '#'])
+
+
+def expand_horizontally(universum: list) -> list:
+    """duplicate elements in rows"""
+    by_vertical = {}
+    for i, row in enumerate(universum):
+        for j, cell in enumerate(row):
+            if by_vertical.get(j) is None:
+                by_vertical[j] = [cell]
+                continue
+
+            by_vertical[j].append(cell)
+
+    columns_to_expand = []
+    for key, vertical in by_vertical.items():
+        if has_galaxies(vertical) is False:
+            columns_to_expand.append(key)
+            continue
+
+    for r, row_to_expand in enumerate(universum):
+        expand_row(row_to_expand, columns_to_expand)
+
+    return universum
+
+
+def expand_vertically(universum: list) -> list:
+    """duplicate row"""
+    rows_to_expand = []
+    for i, row in enumerate(universum):
+        if has_galaxies(row) is False:
+            rows_to_expand.append(i)
+
+    empty_row = ['.' for _ in range(0, len(universum[0]))]
+
+    for i, expand in enumerate(rows_to_expand):
+        universum.insert(expand+i, empty_row)
+
+    return universum
 
 
 def expand_universum(galaxy: list) -> list:
-    """duplicate horizontally row"""
-    for row in galaxy:
-        pass
-
+    galaxy = expand_horizontally(galaxy)
+    galaxy = expand_vertically(galaxy)
     return galaxy
 
 
-assert expand_universum(test_galaxy) == parse(load_data('test_input_expanded.txt'))
+pprint(object=parse(load_data('test_input_expanded.txt')), width=100)
+pprint(object=expand_universum(test_galaxy), width=100)
