9d4616de26598193c5806e77ad6481619b4f2905
MarcinGladkowski <marcingladkowski@gmail.com>
2024-02-26 15:52:27 +0100

    day_15(part_1): resolved

--- day_15/test_main.py
+++ day_15/test_main.py
@@ -1,4 +1,5 @@
 from main import calculate_hash, get_result, calculate_from_instruction
+from shared.main import load_data
 
 test_input = 'rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'
 
@@ -11,8 +12,10 @@ assert 52 == next(generator)
 
 assert 52 == get_result('HASH')
 
-### test input
+"""test input"""
 assert 30 == get_result('rn=1')
 
 assert 1320 == calculate_from_instruction(test_input)
 
+"""part 1"""
+assert 507291 == calculate_from_instruction(load_data('input_data.txt')[0])
