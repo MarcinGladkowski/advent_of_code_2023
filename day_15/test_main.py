080c4c671d93af913c18691283406dbb90b3ac71
MarcinGladkowski <marcingladkowski@gmail.com>
2024-02-27 13:10:08 +0100

    day_15(part_2): recognize box number

--- day_15/test_main.py
+++ day_15/test_main.py
@@ -19,3 +19,6 @@ assert 1320 == calculate_from_instruction(test_input)
 
 """part 1"""
 assert 507291 == calculate_from_instruction(load_data('input_data.txt')[0])
+
+"""part 2"""
+assert 3 == get_result('pc') # box number
